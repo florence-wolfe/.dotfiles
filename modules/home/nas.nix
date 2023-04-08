4e63b65432e899e8f9ebb4e8c45d457d359f9ceb
Frank Robert <frankie.robert+github@gmail.com>
2023-04-07 21:36:40 -0400

    comment out the ld link

--- modules/home/nas.nix
+++ modules/home/nas.nix
@@ -29,12 +29,12 @@
   };
   systemd = {
     user = {
-      tmpfiles = {
-        rules = [
-          "L+ /lib/${builtins.baseNameOf pkgs.stdenv.cc.bintools.dynamicLinker} - - - - ${pkgs.stdenv.cc.bintools.dynamicLinker}"
-          "L+ /lib64 - - - - /lib"
-        ];
-      };
+      # tmpfiles = {
+      #   rules = [
+      #     "L+ /lib/${builtins.baseNameOf pkgs.stdenv.cc.bintools.dynamicLinker} - - - - ${pkgs.stdenv.cc.bintools.dynamicLinker}"
+      #     "L+ /lib64 - - - - /lib"
+      #   ];
+      # };
     };
   };
 }
