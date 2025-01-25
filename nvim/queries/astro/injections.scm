1229eece6c43fc1962fba5ffeb571e7ab8212277
Flo Wolfe <florencewolfe@pm.me>
2025-01-25 15:40:12 -0500

    add support for @ attributes and templtes

--- nvim/queries/astro/injections.scm
+++ nvim/queries/astro/injections.scm
@@ -4,6 +4,16 @@
   (attribute_name) @_alpine_attr
   (quoted_attribute_value 
     (attribute_value) @injection.content)
-  (#match? @_alpine_attr "^x-")
+  (#match? @_alpine_attr "^x-|^\\@")
   (#set! injection.language "javascript"))
+
+(attribute 
+  (attribute_name) @_alpine_attr
+  (attribute_interpolation
+    (attribute_js_expr) @injection.content
+  )
+  (#match? @_alpine_attr "^x-|^\\@")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "javascript")
+)
 
