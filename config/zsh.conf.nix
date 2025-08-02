d53d5e5fb14788d0e89b39add579e13dbc855e0d
Flo Wolfe <florencewolfe@pm.me>
2025-08-02 10:57:51 -0400

    update darwin command

--- config/zsh.conf.nix
+++ config/zsh.conf.nix
@@ -1,23 +1,29 @@
-{ config, ... }: {
+{ config, ... }:
+{
   programs = {
     zsh = {
       enable = true;
-      autosuggestion = { enable = true; };
+      autosuggestion = {
+        enable = true;
+      };
       enableCompletion = true;
-      syntaxHighlighting = { enable = true; };
+      syntaxHighlighting = {
+        enable = true;
+      };
       shellAliases = {
         "dots" = "cd ~/.dotfiles";
         "edit-dots" = "nvim ~/.dotfiles/";
         "get-profile" = ''echo "$(whoami)@$(hostname)"'';
-        "hm-build" = ''
-          nix build --no-link "$HOME/.dotfiles#homeConfigurations."$(get-profile)".activationPackage"'';
+        "hm-build" =
+          ''nix build --no-link "$HOME/.dotfiles#homeConfigurations."$(get-profile)".activationPackage"'';
         # "$(nix path-info <flake-uri>#homeConfigurations.jdoe.activationPackage)"/activate
         #  "hm-activate" = "(nix path-info \"$HOME/.dotfiles#homeConfigurations.\"$(get-profile)\".activationPackage)/activate\")";
         "hm-update" = ''home-manager switch --flake "$HOME/.dotfiles"'';
-        "d-update" = ''
-          darwin-rebuild switch --flake "$HOME/.dotfiles#\"$(get-profile)\""'';
+        "d-update" = ''darwin-rebuild switch --flake "$HOME/.dotfiles"'';
       };
-      oh-my-zsh = { enable = true; };
+      oh-my-zsh = {
+        enable = true;
+      };
       initExtra = ''
         # conditionally ues secrets
         [[ -f $HOME/.dotfiles/secrets/.env ]] && source $HOME/.dotfiles/secrets/.env
